0b03e79ce6a84e13a560b528fc479ba84626e7c7
Chris Gaona <thebeloved88@sbcglobal.net>
2017-12-09 12:38:24 -0800

    Creates service to handle downloading of song

--- app/src/main/java/com/chrisgaona/musicmachine/DownloadHandler.java
+++ app/src/main/java/com/chrisgaona/musicmachine/DownloadHandler.java
@@ -13,21 +13,6 @@ public class DownloadHandler extends Handler {
 
     @Override
     public void handleMessage(Message msg) {
-        downloadSong(msg.obj.toString());
-    }
-
-    private void downloadSong(String song) {
-        // return difference in milliseconds between current time and epoch
-        long endTime = System.currentTimeMillis() + 10 * 1000;
-
-        while (System.currentTimeMillis() < endTime) {
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-
-        Log.d(TAG, song + " downloaded!");
+//        downloadSong(msg.obj.toString());
     }
 }
--- app/src/main/java/com/chrisgaona/musicmachine/MainActivity.java
+++ app/src/main/java/com/chrisgaona/musicmachine/MainActivity.java
@@ -1,5 +1,6 @@
 package com.chrisgaona.musicmachine;
 
+import android.content.Intent;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,6 +12,7 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
+    public static final String KEY_SONG = "SONG";
     private Button mDownloadButton;
 
     @Override
@@ -18,9 +20,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final DownloadThread thread = new DownloadThread();
-        thread.setName("DownloadThread");
-        thread.start();
+//        final DownloadThread thread = new DownloadThread();
+//        thread.setName("DownloadThread");
+//        thread.start();
 
         mDownloadButton = findViewById(R.id.downloadButton);
 
@@ -31,12 +33,17 @@ public class MainActivity extends AppCompatActivity {
 
                 // send messages to Handler for processing
                 for (String song : Playlist.songs) {
-                    // android keeps a pool of message objects for us to use
-                    Message message = Message.obtain();
-                    // add any type of object to message
-                    // send message to handler
-                    message.obj = song;
-                    thread.mHandler.sendMessage(message);
+//                    // android keeps a pool of message objects for us to use
+//                    Message message = Message.obtain();
+//                    // add any type of object to message
+//                    // send message to handler
+//                    message.obj = song;
+//                    thread.mHandler.sendMessage(message);
+
+                    // start service for each song
+                    Intent intent = new Intent(MainActivity.this, DownloadService.class);
+                    intent.putExtra(KEY_SONG, song);
+                    startService(intent);
                 }
             }
         });
