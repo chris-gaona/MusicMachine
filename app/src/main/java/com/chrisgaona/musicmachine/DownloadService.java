7b54a3a215aacb357cd1570058ad7daf0a838142
Chris Gaona <thebeloved88@sbcglobal.net>
2017-12-11 09:03:13 -0800

    Adds a separate thread to our service

--- app/src/main/java/com/chrisgaona/musicmachine/DownloadService.java
+++ app/src/main/java/com/chrisgaona/musicmachine/DownloadService.java
@@ -3,6 +3,7 @@ package com.chrisgaona.musicmachine;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.os.Message;
 import android.support.annotation.Nullable;
 import android.util.Log;
 
@@ -13,27 +14,28 @@ import android.util.Log;
 public class DownloadService extends Service {
 
     private static final String TAG = DownloadService.class.getSimpleName();
+    private DownloadHandler mHandler;
+
+    @Override
+    public void onCreate() {
+        DownloadThread thread = new DownloadThread();
+        thread.setName("DownloadThread");
+        thread.start();
+        // uses this while loop to prevent null pointer exception from being thrown
+        // not best practice though
+        while (thread.mHandler == null) {
+
+        }
+        mHandler = thread.mHandler;
+    }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         String song = intent.getStringExtra(MainActivity.KEY_SONG);
-        downloadSong(song);
+        Message message = Message.obtain();
+        message.obj = song;
+        mHandler.sendMessage(message);
         return Service.START_REDELIVER_INTENT;
-    }
-
-    private void downloadSong(String song) {
-        // return difference in milliseconds between current time and epoch
-        long endTime = System.currentTimeMillis() + 10 * 1000;
-
-        while (System.currentTimeMillis() < endTime) {
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-
-        Log.d(TAG, song + " downloaded!");
     }
 
     @Nullable
