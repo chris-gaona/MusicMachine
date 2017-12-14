b1c5cd4eaa5b11f17f7739cfd556cd751c51ee1b
Chris Gaona <thebeloved88@sbcglobal.net>
2017-12-14 08:53:03 -0800

    Starts binding our process together using bind & unbind with ServiceConnections

--- app/src/main/java/com/chrisgaona/musicmachine/MainActivity.java
+++ app/src/main/java/com/chrisgaona/musicmachine/MainActivity.java
@@ -1,6 +1,10 @@
 package com.chrisgaona.musicmachine;
 
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.IBinder;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -13,8 +17,38 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
     public static final String KEY_SONG = "SONG";
+    private boolean mBound = false;
     private Button mDownloadButton;
     private Button mPlayButton;
+
+    // using an anonymous class for ServiceConnection
+    private ServiceConnection mServiceConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
+            mBound = true;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName componentName) {
+            mBound = false;
+        }
+    };
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Intent intent = new Intent(this, PlayerService.class);
+        bindService(intent, mServiceConnection, Context.BIND_AUTO_CREATE);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mBound) {
+            unbindService(mServiceConnection);
+            mBound = false;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,5 +90,7 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+
     }
 }
